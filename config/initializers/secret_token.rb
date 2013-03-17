f11098f4d337102e21aede4bc3a31f85fd26f595
viksit arora <enliven.viksit@gmail.com>
2013-03-16 21:30:13 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtisanMarket::Application.config.secret_token = '5f3891432c14ca3d2eda78775a650ad57392b51ac98b7b6ffbe4ec75259820a73914e0d58e41bb8022bf14927924f736191605b47f11b82776fc602f21919574'

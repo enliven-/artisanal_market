2ed02bc7cb38baa6be05efc9a88689fa817ab78e
viksit arora <enliven.viksit@gmail.com>
2013-03-20 15:29:06 -0400

    adds customer and artisan models via devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ArtisanMarket::Application.routes.draw do
+  devise_for :artisans
+
+  devise_for :customers
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

fa4bef6e76be3e2bcdc2e054a754869db84484c3
viksit arora <enliven.viksit@gmail.com>
2013-05-25 08:21:36 -0400

    intermed state

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
-  attr_accessible :description, :name, :artisan_id, :img_file, :palette_id, :product_category_id, :product_category_attributes, :palette_attributes
+  attr_accessible :description, :name, :artisan_id, :img_file, :palette_id, :product_category_id, 
+                  :product_category_attributes, :palette_attributes, :attribute_layer_attributes, :attribute_attributes
   
   has_attached_file :img_file, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   
@@ -9,20 +10,17 @@ class Project < ActiveRecord::Base
   belongs_to :artisan,  class_name: 'User'
   
   has_many :products
-# <<<<<<< HEAD
-#   has_one  :product_category
-#   has_one  :palette
-#   has_many :design_versions
-# =======
+  has_one  :product_category
+  has_one  :palette
+  has_many :design_versions
 
-  
-  belongs_to :palette
-  
-  belongs_to :product_category
   accepts_nested_attributes_for :product_category
   accepts_nested_attributes_for :palette
+  accepts_nested_attributes_for :attribute_layer
+  accepts_nested_attributes_for :attribute
+
 # >>>>>>> flow
-  has_many :design_versions
+
   
   def artisan_assigned?
     self.artisan_id ? true : false
@@ -34,6 +32,14 @@ class Project < ActiveRecord::Base
   
   def product_category_attributes=(attributes)
     self.product_category = ProductCategory.find_or_create_by_label(attributes[:label])
+  end
+
+  def attribute_layer_attributes=(attributes)
+    self.attribute_layer = AttributeLayer.find_or_create_by_label(attributes[:label])
+  end
+
+  def attribute_attributes=(attributes)
+    self.attribute = Attribute.find_or_create_by_label(attributes[:label])
   end
 
   private
